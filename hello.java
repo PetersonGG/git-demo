84c654bf5f896fd3d961377bcb943b3ded2b7444
reznov <reznov198@163.com>
2022-07-08 18:05:23 +0800

    push hello.java

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ public class hello{
 public static void main(String[] args){
 
 	System.out.println("hello world");
-	System.out.println("ni hao 123456789");
+	System.out.println("ni hao 123456789+123465789646453");
 	
 }
 
